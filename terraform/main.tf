43fcea1c07ef0c6d660a9480aa047d35b8ced38f
mranderson <leon.avshalom@gmail.com>
2023-11-25 23:29:31 +0200

    Working Full Auto Deployment Using Kubernetes - 01

--- terraform/main.tf
+++ terraform/main.tf
@@ -151,26 +151,3 @@ resource "aws_route_table_association" "public_subnet_association" {
   subnet_id      = aws_subnet.my_public_subnet.id
   route_table_id = aws_route_table.public_subnet_route.id
 }
-
-
-
-
-
-
-
-
-resource "aws_acm_certificate" "my_cert" {
-  domain_name       = var.domain_name
-  validation_method = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_acm_certificate_validation" "cert" {
-  certificate_arn         = aws_acm_certificate.my_cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
-}
-
-
--- terraform/route53.tf
+++ terraform/route53.tf
@@ -27,3 +27,17 @@ resource "aws_route53_record" "cert_validation" {
   records = [each.value.value]
   ttl     = 60
 }
+
+resource "aws_acm_certificate" "my_cert" {
+  domain_name       = var.domain_name
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = aws_acm_certificate.my_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
